555f313cae33ed9179124bbfbe6af2049fbebccd
north <xiaobei.quq@gmail.com>
2020-05-13 09:19:04 +0800

    modify navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'North Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      '小北的博客 学会记录所想，就不会忘记所爱。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
